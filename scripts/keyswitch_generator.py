8961fef9fa2d6f058f7f9857c8ba52923149388f
Ryan Rotter <rrotter@users.noreply.github.com>
2022-05-21 12:12:40 -0500

    make choc sockets match datasheet, delete other footprints
    * Deleted all switches except Choc
    * Make choc socket footprint match datasheet specs:
      * Corrected Y positions of Choc socket pads
      * Corrected size of Choc socket pads
      * Modify keyspacing to suit my preferences
      * Revert hotswap holes to factory spec 3.0
    * Renamed .pretty dirs

--- scripts/keyswitch_generator.py
+++ scripts/keyswitch_generator.py
@@ -5,10 +5,16 @@ from KicadModTree.KicadFileHandler import KicadFileHandler
 
 from keycap import Keycap
 
-from switch import StabilizerCherryMX, SwitchAlpsMatias, SwitchCherryMX, \
-                   SwitchHybridCherryMxAlps, SwitchKailhChocV1, \
-                   SwitchHotswapKailh, SwitchHotswapKailhChocV1, SwitchKailhKH, \
-                   SwitchKailhNB, SwitchKailhChocMini
+from switch import SwitchKailhChocV1, \
+                   SwitchHotswapKailhChocV1
+
+# SPACING = 19.05 # Standard 0.75 Inches
+SPACING = 19 # Round (in metric) 19mm common on Preonic and other OLKBs
+
+# CHOC_SPACING_X = 18
+# CHOC_SPACING_Y = 17
+CHOC_SPACING_X = 19
+CHOC_SPACING_Y = 18.5
 
 path3d = '${KICAD6_3RD_PARTY}/3dmodels/' \
          'com_github_perigoso_keyswitch-kicad-library/' \
@@ -24,130 +30,17 @@ keycaps = {
     '1.75u90': {'keycap_type': 'regular', 'width': 1.75, 'rotation': 90},
     '2u': {'keycap_type': 'regular', 'width': 2},
     '2u90': {'keycap_type': 'regular', 'width': 2, 'rotation': 90},
-    '2.25u': {'keycap_type': 'regular', 'width': 2.25},
-    '2.25u90': {'keycap_type': 'regular', 'width': 2.25, 'rotation': 90},
-    '2.5u': {'keycap_type': 'regular', 'width': 2.5},
-    '2.5u90': {'keycap_type': 'regular', 'width': 2.5, 'rotation': 90},
-    '2.75u': {'keycap_type': 'regular', 'width': 2.75},
-    '2.75u90': {'keycap_type': 'regular', 'width': 2.75, 'rotation': 90},
-    '3u': {'keycap_type': 'regular', 'width': 3},
-    '3u90': {'keycap_type': 'regular', 'width': 3, 'rotation': 90},
-    '4u': {'keycap_type': 'regular', 'width': 4},
-    '4.5u': {'keycap_type': 'regular', 'width': 4.5},
-    '5.5u': {'keycap_type': 'regular', 'width': 5.5},
-    '6u': {'keycap_type': 'regular', 'width': 6},
-    '6uOffset': {'keycap_type': 'regular', 'width': 6, 'x_offset': -9.525},
-    '6.25u': {'keycap_type': 'regular', 'width': 6.25},
-    '6.5u': {'keycap_type': 'regular', 'width': 6.5},
-    '7u': {'keycap_type': 'regular', 'width': 7},
-    'ISOEnter': {'keycap_type': 'ISOEnter'},
-    'ISOEnter90': {'keycap_type': 'ISOEnter', 'rotation': 90},
-    'ISOEnter180': {'keycap_type': 'ISOEnter', 'rotation': 180},
-    'ISOEnter270': {'keycap_type': 'ISOEnter', 'rotation': 270}
 }
 
-
-def generate_stabilizer_cherry_mx(output_path):
-    group = 'Mounting_Keyboard_Stabilizer'
-    out_path = os.path.join(output_path, f'{group}.pretty')
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
-    sizes = [2, 3, 6, 6.25, 7, 8]
-    for size in sizes:
-        stab = StabilizerCherryMX(size=size, path3d=path3d)
-        file_handler = KicadFileHandler(stab)
-        file_handler.writeFile(os.path.join(out_path, f'{stab.name}.kicad_mod'), timestamp=0)
-
-
-def generate_switch_alps_matias(output_path):
-    spacing = 19.05
-    group = 'Switch_Keyboard_Alps_Matias'
-    out_path = os.path.join(output_path, f'{group}.pretty')
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.5u', '1.75u', '2u', '2.25u', '2.5u', '2.75u',
-            '3u', '4u', '4.5u', '5.5u', '6u', '6.25u', '6.5u', '7u',
-            'ISOEnter']
-
-    switches = []
-
-    switches.append(SwitchAlpsMatias(path3d=path3d))
-
-    for key in keys:
-        switches.append(SwitchAlpsMatias(path3d=path3d,
-                                         keycap=Keycap(spacing=spacing,
-                                                       **keycaps[key])))
-
-    for switch in switches:
-        file_handler = KicadFileHandler(switch)
-        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
-
-
-def generate_switch_cherry_mx(output_path):
-    spacing = 19.05
-    group = 'Switch_Keyboard_Cherry_MX'
-    out_path = os.path.join(output_path, f'{group}.pretty')
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.25u90', '1.5u', '1.5u90', '1.75u', '1.75u90',
-            '2u', '2u90', '2.25u', '2.25u90', '2.5u', '2.5u90', '2.75u',
-            '2.75u90', '3u', '3u90', '4u', '4.5u', '5.5u', '6u', '6uOffset',
-            '6.25u', '6.5u', '7u', 'ISOEnter', 'ISOEnter90', 'ISOEnter180',
-            'ISOEnter270']
-
-    switches = []
-
-    for switch_type in ['PCB', 'Plate']:
-        switches.append(SwitchCherryMX(switch_type=switch_type, path3d=path3d))
-
-        for key in keys:
-            switches.append(SwitchCherryMX(switch_type=switch_type,
-                                           path3d=path3d,
-                                           keycap=Keycap(spacing=spacing,
-                                                         **keycaps[key])))
-
-    for switch in switches:
-        file_handler = KicadFileHandler(switch)
-        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
-
-
-def generate_switch_hybrid_cherry_mx_alps(output_path):
-    spacing = 19.05
-    group = 'Switch_Keyboard_Hybrid'
-    out_path = os.path.join(output_path, f'{group}.pretty')
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.5u', '1.75u', '2u', '2.25u', '2.5u', '2.75u',
-            '3u', '4u', '4.5u', '5.5u', '6u', '6.25u', '6.5u', '7u',
-            'ISOEnter']
-
-    switches = []
-
-    switches.append(SwitchHybridCherryMxAlps(path3d=path3d))
-
-    for key in keys:
-        switches.append(
-            SwitchHybridCherryMxAlps(path3d=path3d,
-                                     keycap=Keycap(spacing=spacing,
-                                                   **keycaps[key])))
-
-    for switch in switches:
-        file_handler = KicadFileHandler(switch)
-        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
-
-
 def generate_switch_kailh_choc_v1(output_path):
-    x_spacing = 18
-    y_spacing = 17
-    group = 'Switch_Keyboard_Kailh'
+    x_spacing = 19
+    y_spacing = 18
+    # group = 'Switch_Keyboard_Kailh'
+    group = 'Keyswitch_Choc'
     out_path = os.path.join(output_path, f'{group}.pretty')
     if not os.path.isdir(out_path):
         os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.25u90', '1.5u', '1.5u90', '1.75u', '1.75u90',
-            '2u', '2u90', '2.25u', '2.25u90', '2.5u', '2.5u90', '2.75u',
-            '2.75u90', '3u', '3u90', '4u', '4.5u', '5.5u', '6u', '6uOffset',
-            '6.25u', '6.5u', '7u', 'ISOEnter', 'ISOEnter90', 'ISOEnter180',
-            'ISOEnter270']
+    keys = ['1u', '1.25u', '1.5u', '2u']
 
     switches = []
 
@@ -164,142 +57,29 @@ def generate_switch_kailh_choc_v1(output_path):
         file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
 
 
-def generate_switch_kailh_choc_mini(output_path):
-    x_spacing = 18
-    y_spacing = 17
-    group = 'Switch_Keyboard_Kailh'
-    out_path = os.path.join(output_path, f'{group}.pretty')
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.25u90', '1.5u', '1.5u90', '1.75u', '1.75u90',
-            '2u', '2u90', '2.25u', '2.25u90', '2.5u', '2.5u90', '2.75u',
-            '2.75u90', '3u', '3u90', '4u', '4.5u', '5.5u', '6u', '6uOffset',
-            '6.25u', '6.5u', '7u', 'ISOEnter', 'ISOEnter90', 'ISOEnter180',
-            'ISOEnter270']
-
-    switches = []
-
-    switches.append(SwitchKailhChocMini(path3d=path3d))
-
-    for key in keys:
-        switches.append(SwitchKailhChocMini(path3d=path3d,
-                                          keycap=Keycap(x_spacing=x_spacing,
-                                                        y_spacing=y_spacing,
-                                                        **keycaps[key])))
-
-    for switch in switches:
-        file_handler = KicadFileHandler(switch)
-        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
-
-
-def generate_switch_kailh_kh(output_path):
-    spacing = 19.05
-    group = 'Switch_Keyboard_Kailh'
-    out_path = os.path.join(output_path, f'{group}.pretty')
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.25u90', '1.5u', '1.5u90', '1.75u', '1.75u90',
-            '2u', '2u90', '2.25u', '2.25u90', '2.5u', '2.5u90', '2.75u',
-            '2.75u90', '3u', '3u90', '4u', '4.5u', '5.5u', '6u', '6uOffset',
-            '6.25u', '6.5u', '7u', 'ISOEnter', 'ISOEnter90', 'ISOEnter180',
-            'ISOEnter270']
-
-    switches = []
-
-    switches.append(SwitchKailhKH(path3d=path3d))
-
-    for key in keys:
-        switches.append(SwitchKailhKH(path3d=path3d,
-                                      keycap=Keycap(spacing=spacing,
-                                                    **keycaps[key])))
-
-    for switch in switches:
-        file_handler = KicadFileHandler(switch)
-        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
-
-
-def generate_switch_kailh_nb(output_path):
-    spacing = 19.05
-    group = 'Switch_Keyboard_Kailh'
-    out_path = os.path.join(output_path, f'{group}.pretty')
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.5u', '1.75u', '2u', '2.25u', '2.5u', '2.75u',
-            '3u', '4u', '4.5u', '5.5u', '6u', '6.25u', '6.5u', '7u',
-            'ISOEnter']
-
-    switches = []
-
-    switches.append(SwitchKailhNB(path3d=path3d))
-
-    for key in keys:
-        switches.append(SwitchKailhNB(path3d=path3d,
-                                      keycap=Keycap(spacing=spacing,
-                                                    **keycaps[key])))
-
-    for switch in switches:
-        file_handler = KicadFileHandler(switch)
-        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
-
-
-def generate_switch_hotswap_kailh(output_path):
-    spacing = 19.05
-    group = 'Switch_Keyboard_Hotswap_Kailh'
-    out_path = os.path.join(output_path, f'{group}.pretty')
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.25u90', '1.5u', '1.5u90', '1.75u', '1.75u90',
-            '2u', '2u90', '2.25u', '2.25u90', '2.5u', '2.5u90', '2.75u',
-            '2.75u90', '3u', '3u90', '4u', '4.5u', '5.5u', '6u', '6uOffset',
-            '6.25u', '6.5u', '7u', 'ISOEnter', 'ISOEnter90', 'ISOEnter180',
-            'ISOEnter270']
-
-    switches = []
-
-    for plated in [False, True]:
-
-        switches.append(SwitchHotswapKailh(path3d=path3d, plated_th=plated))
-
-        for key in keys:
-            switches.append(SwitchHotswapKailh(path3d=path3d,
-                                               plated_th=plated,
-                                               keycap=Keycap(spacing=spacing,
-                                                             **keycaps[key])))
-
-    for switch in switches:
-        file_handler = KicadFileHandler(switch)
-        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
-
-
 def generate_switch_hotswap_kailh_choc_v1(output_path):
-    x_spacing = 18
-    y_spacing = 17
-    group = 'Switch_Keyboard_Hotswap_Kailh'
+    x_spacing = 19
+    y_spacing = 18
+    # group = 'Switch_Keyboard_Hotswap_Kailh'
+    group = 'Keyswitch_Choc_Socket'
     out_path = os.path.join(output_path, f'{group}.pretty')
     if not os.path.isdir(out_path):
         os.mkdir(out_path)
-    keys = ['1u', '1.25u', '1.25u90', '1.5u', '1.5u90', '1.75u', '1.75u90',
-            '2u', '2u90', '2.25u', '2.25u90', '2.5u', '2.5u90', '2.75u',
-            '2.75u90', '3u', '3u90', '4u', '4.5u', '5.5u', '6u', '6uOffset',
-            '6.25u', '6.5u', '7u', 'ISOEnter', 'ISOEnter90', 'ISOEnter180',
-            'ISOEnter270']
+    keys = ['1u', '1.25u', '1.5u', '2u']
 
     switches = []
 
-    for plated in [False, True]:
+    switches.append(SwitchHotswapKailhChocV1(path3d=path3d))
 
-        switches.append(SwitchHotswapKailhChocV1(path3d=path3d, plated_th=plated))
+    for key in keys:
+        switches.append(SwitchHotswapKailhChocV1(path3d=path3d,
+                                                 keycap=Keycap(x_spacing=x_spacing,
+                                                               y_spacing=y_spacing,
+                                                               **keycaps[key])))
 
-        for key in keys:
-            switches.append(SwitchHotswapKailhChocV1(path3d=path3d,
-                                                     plated_th=plated,
-                                                     keycap=Keycap(x_spacing=x_spacing,
-                                                                   y_spacing=y_spacing,
-                                                                   **keycaps[key])))
-
-        for switch in switches:
-            file_handler = KicadFileHandler(switch)
-            file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
+    for switch in switches:
+        file_handler = KicadFileHandler(switch)
+        file_handler.writeFile(os.path.join(out_path, f'{switch.name}.kicad_mod'), timestamp=0)
 
 
 if __name__ == '__main__':
@@ -318,13 +98,5 @@ if __name__ == '__main__':
     if not os.path.isdir(args.output):
         os.mkdir(args.output)
 
-    generate_stabilizer_cherry_mx(args.output)
-    generate_switch_alps_matias(args.output)
-    generate_switch_cherry_mx(args.output)
-    generate_switch_hybrid_cherry_mx_alps(args.output)
     generate_switch_kailh_choc_v1(args.output)
-    generate_switch_kailh_choc_mini(args.output)
-    generate_switch_kailh_kh(args.output)
-    generate_switch_kailh_nb(args.output)
-    generate_switch_hotswap_kailh(args.output)
     generate_switch_hotswap_kailh_choc_v1(args.output)
